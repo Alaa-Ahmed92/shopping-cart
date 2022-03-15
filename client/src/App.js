8e97937c3ccd8493f6bb16cdfc0e3b98242ba744
Alaa <alaa.ahmed.a92@gmail.com>
2022-03-15 15:03:20 +0000

    Add Product Details

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Products from './components/Products/Products';
 import { messages } from './messages';
 import data from './data.json';
 import './App.css';
-import Products from './components/Products/Products';
 
 class App extends Component {
   state = {
     products: data
   }
   render() {
-    // console.log(data);
     return (
       <div className="layout">
         <Header />
